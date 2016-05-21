285b16cb1ac62a240d428a1eb54076a69c143866
zouzhigang <zouzhigang@loopeer.com>
2016-05-21 14:03:30 +0800

    Initial Commit

---
+++ ZZGPhotoGroup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZGPhotoGroup
+//
+//  Created by dito on 16/5/21.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZZGPhotoGroupTests/ZZGPhotoGroupTests.m
@@ -0,0 +1,39 @@
+//
+//  ZZGPhotoGroupTests.m
+//  ZZGPhotoGroupTests
+//
+//  Created by dito on 16/5/21.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZZGPhotoGroupTests : XCTestCase
+
+@end
+
+@implementation ZZGPhotoGroupTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
